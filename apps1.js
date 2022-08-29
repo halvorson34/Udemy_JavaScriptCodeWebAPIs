347219584f197f4101681d25967a6ebca89f88cc
[Matt Halvorson] <[matt.halvorson@hrblock.com]>
2022-08-29 14:31:10 -0500

    apps1.js

--- apps1.js
+++ apps1.js
@@ -1,1 +1,12 @@
-const obj1 = {};
+const obj1 = {
+    "first" : "Laurence",
+    "last" : "Svekis",
+    "num" : 100,
+    "bool" : false,
+    "null" : null,
+    "arr" : [],
+    "obj" : {}
+};
+console.log(obj1);
+console.log(obj1.bool);
+console.log(obj1["bool"]);
