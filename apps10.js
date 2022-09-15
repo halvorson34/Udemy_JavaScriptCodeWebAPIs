05bcc9d97fe44bdaf18dc80d2f99b307feef09ea
[Matt Halvorson] <[matt.halvorson@hrblock.com]>
2022-09-15 11:24:39 -0500

    apps10.js end

--- apps10.js
+++ apps10.js
@@ -1,3 +1,27 @@
 const btn = document.querySelector(".btn");
 const btn1 = document.querySelector(".btn1");
 const output = document.querySelector(".output");
+
+const url = 'https://www.reddit.com/r/funny/top/.json?limit=5';
+
+btn.onclick = () => {
+    fetch(url)
+    .then(rep => rep.json())
+    .then(data => {
+        getData(data.data.children);
+    })
+}
+
+function getData(data) {
+    console.log(data);
+    let html = '';
+    data.forEach((item) => {
+        console.log(item.data);
+        const el = item.data;
+        html += `<div><h3>${el.title}</h3>`;
+        html += `<img src = '${el.thumbnail}'>`;
+        html += `<br>`;
+        html += `<a href = '${el.url}' target = "_blank">${el.url}</a></div>`;
+    });
+    output.innerHTML = html;
+}
