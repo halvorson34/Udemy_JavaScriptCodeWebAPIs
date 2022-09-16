060088e8d6952331a4dc7b7910090738cfdcd05a
[Matt Halvorson] <[matt.halvorson@hrblock.com]>
2022-09-16 11:18:24 -0500

    apps11.js

--- apps11.js
+++ apps11.js
@@ -1,3 +1,13 @@
 const btn = document.querySelector(".btn");
 const btn1 = document.querySelector(".btn1");
 const output = document.querySelector(".output");
+const url = 'https://api.github.com/zen';
+
+btn.onclick = () => {
+    fetch(url)
+    .then(res => res.text())
+    .then(zen => {
+        console.log(zen);
+        output.innerHTML = `<h1>${zen}</h1>`;
+    })
+}
